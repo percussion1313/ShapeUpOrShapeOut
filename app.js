756869da64f1718e513d37250dea777af18b50e1
percussion1313 <peterjacobgrant@gmail.com>
2018-06-07 16:09:32 -0500

    First commit. Added HTML and CSS elements

---
+++ app.js
@@ -0,0 +1,29 @@
+class Shape {
+    constructor() {
+        this.shapeColor = shapeColor
+    }
+}
+
+class Rectangle extends Shape {
+    constructor() {
+        
+    }
+}
+
+class Square extends Shape {
+    constructor() {
+        
+    }
+}
+
+class Circle extends Shape {
+    constructor() {
+        
+    }
+}
+
+class IsoscelesRightTriangle extends Shape {
+    constructor() {
+        
+    }
+}
